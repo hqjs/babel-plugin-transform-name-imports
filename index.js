0005eaac3bc111c1908ba09d57cd275d0db90439
hqjs <hqjsorg@gmail.com>
2019-07-28 19:51:12 +0300

    Add vue style @ imports, add more system module polyfills

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
-// TODO: complete mapping https://github.com/webpack/node-libs-browser
+// NOTE: complete mapping https://github.com/webpack/node-libs-browser
 // TODO: some names remain the same, but require library to be installed e.g. assert, buffer, events
+// TODO: serve this modules from hq instead of project root
 const BUILT_IN = {
+  console: 'console-browserify',
+  constants: 'constants-browserify',
+  crypto: 'crypto-browserify',
+  domain: 'domain-browser',
   http: 'stream-http',
   https: 'https-browserify',
+  os: 'os-browserify',
+  path: 'path-browserify',
+  querystring: 'querystring-es3',
+  stream: 'stream-browserify',
+  sys: 'util',
+  tty: 'tty-browserify',
   zlib: 'browserify-zlib',
 };
 
 const NAME_PATTERN_MODULE = /((@[\w-.]+\/)?[\w-.]+)(.*)/;
 
 const resolveModulePath = (resolve, value) => {
-  const [ , module, , rest ] = NAME_PATTERN_MODULE.exec(value);
+  const [, module, , rest] = NAME_PATTERN_MODULE.exec(value);
   const name = rest === '' ?
     resolve[module] || BUILT_IN[module] || module :
     BUILT_IN[module] || module;
   return `/node_modules/${name}${rest}`;
 };
 
-const notNameImport = modName => (/^\.{0,2}\//).test(modName) || (/^https?:\/\//).test(modName);
+const notNameImport = modName => (/^\.{0,2}\//).test(modName) ||
+  (/^https?:\/\//).test(modName) ||
+  (/^@\//).test(modName);
 
 // TODO handle dynamic require
 const notRequire = (t, nodePath) => {
-  const [ requireArg, ...rest ] = nodePath.node.arguments;
+  const [requireArg, ...rest] = nodePath.node.arguments;
   return nodePath.node.callee.name !== 'require' ||
     rest.length !== 0 ||
     !t.isStringLiteral(requireArg) ||
     nodePath.scope.hasBinding('require');
 };
 
-module.exports = function({ types: t }) {
+module.exports = function ({ types: t }) {
   return {
     visitor: {
       CallExpression(nodePath, state) {
         const resolve = (state.opts && state.opts.resolve) || {};
         const { node } = nodePath;
         if (notRequire(t, nodePath)) return;
-        const [ requireArg ] = node.arguments;
+        const [requireArg] = node.arguments;
         const { value: modName } = requireArg;
         if (notNameImport(modName)) return;
         requireArg.value = resolveModulePath(resolve, modName);
